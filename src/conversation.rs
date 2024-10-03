c4deea5cfcf5174d8fa6535e8e4bedc41dd38a38
Andrew <lyerdestroyer@gmail.com>
2024-10-03 16:07:20 +0800

    Added file upload

---
+++ src/conversation.rs
@@ -0,0 +1,152 @@
+use std::error::Error;
+
+use reqwest::{multipart, Body, Client};
+use serde::Serialize;
+use serde_json::Value;
+use tokio::fs::File;
+use tokio_util::codec::{BytesCodec, FramedRead};
+
+const CONVERSATION_BASE_URL: &str = "https://conversations.twilio.com/v1";
+const CONVERSATION_MEDIA_URL: &str = "https://mcs.us1.twilio.com/v1";
+
+const TWILIO_ACCOUNT_SID: &str = "TWILIO_ACCOUNT_SID";
+const TWILIO_AUTH_TOKEN: &str = "TWILIO_AUTH_TOKEN";
+
+pub struct Conversation {
+    client: Client,
+
+    username: String,
+    password: String,
+}
+
+impl Conversation {
+    pub fn new() -> Self {
+        let twilio_account_sid =
+            std::env::var(TWILIO_ACCOUNT_SID).expect("Missing TWILIO_ACCOUNT_SID env");
+        let twilio_auth_token =
+            std::env::var(TWILIO_AUTH_TOKEN).expect("Missing TWILIO_AUTH_TOKEN env");
+
+        Self {
+            client: reqwest::Client::new(),
+            username: twilio_account_sid,
+            password: twilio_auth_token,
+        }
+    }
+
+    pub async fn create_conversation<T>(&self, params: &T) -> Result<Value, Box<dyn Error>>
+    where
+        T: Serialize + ?Sized,
+    {
+        let res = self
+            .client
+            .post(format!("{CONVERSATION_BASE_URL}/Conversations"))
+            .basic_auth(self.username.clone(), Some(self.password.clone()))
+            .form(&params)
+            .send()
+            .await?;
+
+        let result = res.text().await?;
+        let result: Value = serde_json::from_str(&result)?;
+
+        Ok(result)
+    }
+
+    pub async fn delete_conversation(&self, sid: String) -> Result<(), Box<dyn Error>> {
+        self.client
+            .delete(format!("{CONVERSATION_BASE_URL}/Conversations/{sid}"))
+            .basic_auth(self.username.clone(), Some(self.password.clone()))
+            .send()
+            .await?;
+
+        Ok(())
+    }
+
+    pub async fn create_message<T>(
+        &self,
+        conversation_sid: String,
+        params: &T,
+    ) -> Result<Value, Box<dyn Error>>
+    where
+        T: Serialize + ?Sized,
+    {
+        let url = format!("{CONVERSATION_BASE_URL}/Conversations/{conversation_sid}/Messages");
+        let res = self
+            .client
+            .post(url)
+            .basic_auth(self.username.clone(), Some(self.password.clone()))
+            .form(&params)
+            .basic_auth(self.username.clone(), Some(self.password.clone()))
+            .send()
+            .await?;
+
+        let result = res.text().await?;
+        let result: Value = serde_json::from_str(&result)?;
+
+        Ok(result)
+    }
+
+    pub async fn delete_message(
+        &self,
+        conversation_sid: String,
+        message_sid: String,
+    ) -> Result<(), Box<dyn Error>> {
+        let url = format!(
+            "{CONVERSATION_BASE_URL}/Conversations/{conversation_sid}/Messages/{message_sid}"
+        );
+
+        self.client
+            .delete(url)
+            .basic_auth(self.username.clone(), Some(self.password.clone()))
+            .send()
+            .await?;
+
+        Ok(())
+    }
+
+    pub async fn list_messages(&self, conversation_sid: String) -> Result<Value, Box<dyn Error>> {
+        let url = format!("{CONVERSATION_BASE_URL}/Conversations/{conversation_sid}/Messages");
+        let res = self
+            .client
+            .get(url)
+            .basic_auth(self.username.clone(), Some(self.password.clone()))
+            .send()
+            .await?;
+
+        let result = res.text().await?;
+        let result: Value = serde_json::from_str(&result)?;
+
+        Ok(result)
+    }
+
+    pub async fn upload_media(
+        &self,
+        service_sid: String,
+        file_path: String,
+        file_name: String,
+    ) -> Result<Value, Box<dyn Error>> {
+        let url = format!("{CONVERSATION_MEDIA_URL}/Services/{service_sid}/Media");
+        let file = File::open(file_path).await?;
+
+        let stream = FramedRead::new(file, BytesCodec::new());
+        let file_body = Body::wrap_stream(stream);
+
+        let file_part = multipart::Part::stream(file_body).file_name(file_name);
+
+        let form = multipart::Form::new().part("file", file_part);
+
+        let res = self
+            .client
+            .post(url)
+            .basic_auth(self.username.clone(), Some(self.password.clone()))
+            .multipart(form)
+            .send()
+            .await?;
+
+        let result = res.text().await?;
+        let result: Value = serde_json::from_str(&result)?;
+
+        Ok(result)
+    }
+
+    
+}
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,51 @@
 use std::error::Error;
 
-const TWILIO_ACCOUNT_SID: &str = "TWILIO_ACCOUNT_SID";
-const TWILIO_AUTH_TOKEN: &str = "TWILIO_AUTH_TOKEN";
+use conversation::Conversation;
 
-const CONVERSATION_BASE_URL: &str = "https://conversations.twilio.com/v1";
+mod conversation;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let twilio_account_sid =
-        std::env::var(TWILIO_ACCOUNT_SID).expect("Missing TWILIO_ACCOUNT_SID env");
-    let twilio_auth_token =
-        std::env::var(TWILIO_AUTH_TOKEN).expect("Missing TWILIO_AUTH_TOKEN env");
+    // let params = [("FriendlyName", "Rust Room")];
+    // let params = serde_json::json!({
+    //     "FriendlyName": "Another Rust Room"
+    // });
 
-    let client = reqwest::Client::new();
-    let params = [("FriendlyName", "Rust Room")];
+    let conversation = Conversation::new();
 
-    let res = client
-        .post(format!("{CONVERSATION_BASE_URL}/Conversations"))
-        .basic_auth(twilio_account_sid, Some(twilio_auth_token))
-        .form(&params)
-        .send()
+    // let new_conversation = conversation.create_conversation(&params).await?;
+
+    // println!("Result: {new_conversation}");
+
+    // let _ = conversation
+    //     .delete_conversation("CHeb2ae7ba9c654c36a538f918151296f3".to_owned())
+    //     .await?;
+
+    // println!("Result: {result}");
+
+    // let params = serde_json::json!({
+    //     "Author": "some rando",
+    //     "Body": "Another one"
+    // });
+    // let _message = conversation.create_message("CH7ab92711322841599af2dfd59f5e9631".to_owned(), &params).await?;
+
+    // println!("Message: {message}");
+    // let message_list = conversation.list_messages("CH7ab92711322841599af2dfd59f5e9631".to_owned()).await?;
+    // println!("Messages: {message_list}");
+
+    // conversation
+    //     .delete_message(
+    //         "CH7ab92711322841599af2dfd59f5e9631".to_owned(),
+    //         "IMef8b7764ea4e4bcdb45c15d3d1438c9c".to_owned(),
+    //     )
+    //     .await?;
+    let result = conversation
+        .upload_media(
+            "IS3ff2c2d08e3d42d7bce007de1e4e740c".to_owned(),
+            "/home/wsl/projects/rav-chat/R.jpg".to_owned(),
+            "cat.png".to_owned(),
+        )
         .await?;
-
-    let result = res.text().await?;
 
     println!("Result: {result}");
 
